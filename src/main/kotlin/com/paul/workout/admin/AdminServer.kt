d01841fd7671a9c517ee4593ce94fa0e70bd8706
pum001c <murphp15@tcd.ie>
2021-01-25 10:50:58 +0000

    first commit

--- src/main/kotlin/com/paul/workout/admin/AdminServer.kt
+++ src/main/kotlin/com/paul/workout/admin/AdminServer.kt
@@ -3,12 +3,15 @@ package com.paul.workout.admin
 import de.codecentric.boot.admin.server.config.EnableAdminServer
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Configuration
-
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableScheduling
 class SpringBootAdminApplication
 
 fun main(args: Array<String>) {
